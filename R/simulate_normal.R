2d19be0c1436a3066dd4af30ddc32dd6690098b0
Isabella Richmond <52459005+icrichmond@users.noreply.github.com>
2023-10-10 10:56:15 -0400

    simulate_normal_group function

--- R/simulate_normal.R
+++ R/simulate_normal.R
@@ -2,18 +2,3 @@ simulate_normal <- function(n) {
   y <- rnorm(n, mean = 0, sd = 1)
   list(y = y, n = n)
 }
-
-simulate_normal_group <- function(n_per_group, J){
-
-  alpha <- rnorm(J, mean = 0, sd = 3)
-
-  j <- rep(1:J, each = n_per_group)
-
-  y <- rnorm(n_per_group*J, mean = alpha[j], sd = .5)
-  list(
-    n = J * n_per_group,
-    J = J,
-    y = y,
-    j = j
-  )
-}
